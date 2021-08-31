e9d27d20e8f69238935711902df442fc1ffec247
uday7095 <87174745+uday7095@users.noreply.github.com>
2021-08-31 14:16:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 #Create security group with firewall rules
 resource "aws_security_group" "my_security_group" {
   name        = var.security_group
+  vpc         = var.vpc_id
   description = "security group for Ec2 instance"
 
   ingress {
