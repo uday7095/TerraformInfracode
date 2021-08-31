500b229bc76ddec9975abea7e5ddacd2fbf02223
uday7095 <87174745+uday7095@users.noreply.github.com>
2021-08-31 15:34:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "WindowsBox" {
   ami           = "ami-0c1a7f89451184c8b"
   instance_type = "t2.medium"
-  key_name = aws_key_pair.keypair.key_name
+  key_name = "3385"
 
   subnet_id = "subnet-0284d35b71c7cc0b7"
   vpc_security_group_ids = ["sg-0b6e8e9e9cbaaee47"]
@@ -21,7 +21,3 @@ resource "aws_eip" "ip" {
     instance = aws_instance.WindowsBox.id
 }
 
-resource "aws_key_pair" "keypair" {
-  key_name = "WindowsBox-keypair"
-  public_key = file("./kp/WindowsBox-keypair.pub")
-}
